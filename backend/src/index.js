eba0f73435b4bcc91f69bcc990c4381f02e25bec
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-05-22 00:56:34 +0600

    Database connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 7000;
@@ -8,6 +9,11 @@ const PORT = process.env.PORT || 7000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("MongoDB connection error", error));
 
 app.get("/test", async (req, res) => {
   res.json({ message: "Hello from server" });
