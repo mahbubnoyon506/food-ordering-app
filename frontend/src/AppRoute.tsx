f82997ae61b8b324c7fdb31fb182d27aab24dbf0
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-06-11 15:23:38 +0600

    Home page component added

--- frontend/src/AppRoute.tsx
+++ frontend/src/AppRoute.tsx
@@ -1,11 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./global.css";
-import Layout from "./layout/layout";
+import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoute = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>This is home</Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
