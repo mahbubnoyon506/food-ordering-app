a200a09f0d2e2a392d36b42be3c7140a33719f1a
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-06-11 19:15:36 +0600

    Authentication feature added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoute from "./AppRoute.tsx";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoute />
+      <Auth0ProviderWithNavigate>
+        <AppRoute />
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>
 );
