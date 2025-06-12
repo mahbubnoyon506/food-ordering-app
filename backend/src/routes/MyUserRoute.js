f87641485e4a01317e0bb5ab95a2681b9cecdba5
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-06-12 19:07:09 +0600

    JWT token verification added

--- backend/src/routes/MyUserRoute.js
+++ backend/src/routes/MyUserRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const MyUserController = require("../controllers/MyUserController");
+const { jwtCheck } = require("../middleware/auth");
 
 router.get("/", MyUserController.getCurrentUser);
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 module.exports = router;
