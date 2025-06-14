a364ba4bb156a5ba30231a8aa43f747b4f502170
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-06-14 14:37:45 +0600

    Build issue fixed

--- frontend/src/AppRoute.tsx
+++ frontend/src/AppRoute.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./global.css";
-import Layout from "./layout/Layout";
+
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import Layout from "./layout/Layout";
 
 const AppRoute = () => {
   return (
